e54ecbbd1b1d41fe47e2e403b6790dd9716fdc11
Sanan Moinuddin <mohammedsanan18@gmail.com>
2025-09-16 21:43:38 -0400

    next.js issue fix#3

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,8 +1,8 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-export default function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: { cid: string } }>) {
-  const { cid } = params;
+export default async function CoursesLayout(
+  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }>; }>) {
+  const { cid } = await params;
   return (
     <div id="wd-courses">
       <h2>Courses {cid}</h2>
