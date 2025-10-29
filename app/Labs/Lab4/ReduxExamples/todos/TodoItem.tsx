340320f9b9a0bc5a4d0136a997c90ad576571e00
Sanan Moinuddin <mohammedsanan18@gmail.com>
2025-10-29 01:46:56 -0400

    a4 Redux v1.4

--- app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteTodo, setTodo } from './todosReducer';
--- app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoList.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from "react";
 import TodoForm from "./TodoForm";
 import TodoItem from "./TodoItem";
