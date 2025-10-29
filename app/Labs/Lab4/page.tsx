69b4a5c86ad0791a5bc87ab45eb6c4f2469209b8
Sanan Moinuddin <mohammedsanan18@gmail.com>
2025-10-29 01:43:33 -0400

    a4 Redux v1.3

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -11,7 +11,7 @@ import ObjectStateVariable from './ObjectStateVariable';
 import ArrayStateVariable from './ArrayStateVariable';
 import ParentStateComponent from './ParentStateComponent';
 import ReduxExamples from './ReduxExamples/page';
-import store from './store/page';
+import store from './store/store';
 import { Provider } from 'react-redux';
 
 export default function Lab4() {
