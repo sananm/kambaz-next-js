4b633f62656242d4bb2a18fb8079c7072fe57aba
Sanan Moinuddin <mohammedsanan18@gmail.com>
2025-11-11 22:57:32 -0500

    Calendar and inbox fixed

---
+++ app/(Kambaz)/Calendar/page.tsx
@@ -0,0 +1,8 @@
+export default function Calendar() {
+  return (
+    <div className="p-4">
+      <h1>Calendar</h1>
+      <p>Calendar feature coming soon...</p>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Inbox/page.tsx
@@ -0,0 +1,8 @@
+export default function Inbox() {
+  return (
+    <div className="p-4">
+      <h1>Inbox</h1>
+      <p>Inbox feature coming soon...</p>
+    </div>
+  );
+}
